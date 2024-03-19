ef6262007d0c03cf06962f2bc133f998bb05b3d8
MaholisC <159423996+MaholisC@users.noreply.github.com>
2024-03-19 13:12:44 +0100

    Add files via upload

---
+++ II]-DINAMICA/C_Patrones/a-Proyecto/js/cover.js
@@ -0,0 +1,10 @@
+swal({
+    title:'Comenzamos la aventura..',
+    text:'Preparado para tu despegue...',
+    button: false,
+    timer: 3000,
+})
+function redireccionar() {
+    location.href="https://codepen.io/MaholisC";
+    
+}
